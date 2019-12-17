47be7e7d50c1c68479571cc15756663880c6b6bb
nicomanzonelli <nicomanzonelli@gmail.com>
2019-12-17 22:08:50 +0000

    added info panel to app and updated readme

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 Search Twitter for the Derogatory Use of Ordinary Text
 
-<a href = durt.dse-app.com> durt.dse-app.com</a>
+<a href = durt.dse-app.com> durt.dse-apps.com</a>
 
 ## What is DURT?
 
-DURT is a R Shiny application that allows the user to check twitter accounts for potentially unprofessional, derogatory, or otherwise offensive content. It also provides a general look at their overall twitter use. DURT makes a provides a straightforward open source solution designed to allow individuals or organizations to screen twitters.
+DURT is a R Shiny application that allows the user to check twitter accounts for potentially unprofessional, derogatory, or otherwise offensive content. It also gives general insights into a person's twitter use. DURT provides a straightforward open source solution designed to allow individuals or organizations to screen twitters.
 
-This is the repository for the R used to create the app. 
+This repository contains all the code for the app.
+
+You can read more about the app on <a href = 'dscoe.org'> dscoe.org </a> or check the dscoe file. 
--- app/ui.R
+++ app/ui.R
@@ -23,6 +23,8 @@ ui = navbarPage('D.U.R.T.',
                                     h3('Tweet Table'),
                                     DT::dataTableOutput('table1')),
                              column(6, 
+                                    br(),
+                                    br(),
                                     htmlOutput('htmltable')))
                            
                          )),
@@ -30,18 +32,21 @@ ui = navbarPage('D.U.R.T.',
                 tabPanel('Analyze',
                          fluidPage(
                            h1('Account Breakdown', align = 'center'),
-                           h5('Analyze and visualize account DURT and overall Twitter use.', align = 'center'),
+                           h5('Visualize account DURT and overall Twitter use.', align = 'center'),
+                           br(),
                            fluidRow(
                              column(6,
                                     plotOutput('pieplot'),
                                     plotOutput('wordfreq')
                              ),
+                             br(),
                              column(6,
                                     plotOutput('cloud'),
                                     plotOutput('wordfreqd')
                                     
                              ),
                            ),
+                           br(),
                            fluidRow(plotlyOutput('plotly'), width = 1),
                          )),
                 
@@ -50,8 +55,25 @@ ui = navbarPage('D.U.R.T.',
                          h5("Select the the number of tweets you wish to sample and download the raw data.", align = 'center'),
                          fluidRow(column(12, align = 'center', downloadButton("downloadData","Download"))),
                          fluidRow((column(12, align = 'center', sliderInput("slide", "", min = 0, max = 1000, value = 500)))),
-                         h5(""),
-                         DT::DTOutput('tbl') %>% withSpinner())
+                         br(),
+                         DT::DTOutput('tbl') %>% withSpinner()),
+                
+                tabPanel('Info',
+                          h1(tags$b('D.U.R.T.'), align = 'center'),
+                          h5(em('Derogatory Use of Regular Text'), align = 'center'),
+                          br(),
+                          h5("DURT is a R Shiny application that allows the user to check twitter accounts for potentially unprofessional, derogatory, or otherwise offensive content.", align = 'center'),
+                          h5("It also gives general insights into a person's twitter use. DURT provides a straightforward open source solution designed to allow individuals or organizations to screen twitters.", align = 'center'),
+                          br(),
+                          h5('Read more about DURT on dscoe, or visit github/nicomanzonelli/durt to learn how DURT works.', align = 'center'),
+                          br(),
+                          h5(tags$b('Getting errors?'), align = 'center'),
+                          h5('First, check to make sure you searched a Twitter user in the "Dig" panel.
+                            If you have not searched a user on the first tab, the other panels will automatically return errors.', align = 'center'),
+                          h5('If you encounter other errors in the app, please raise the issue on github or contact nico.manzonelli@westpoint.edu', align = "center")
+                         )
+                
+                
                 
 )
 
